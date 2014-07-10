1cb148ce83d795cbf1c30d92864dc6b4f4c14b66
terry <hexawing@163.com>
2014-07-10 15:15:51 +0800

    一个手抖的小bug

--- app/assets/javascripts/app/store/Vendors.js
+++ app/assets/javascripts/app/store/Vendors.js
@@ -1,28 +1,28 @@
 /**
  * 这个是生产厂家联系人的store
  */
+Ext.define('EIM.store.Vendors', {
+    extend: 'Ext.data.Store',
+    model: 'EIM.model.Vendor',
 
-extend: 'Ext.data.Store',
-model: 'EIM.model.Vendor',
+    autoLoad: false,
 
-autoLoad: false,
-
-proxy: {
-    url: 'servlet/GetLogData?type=vendors', //TODO 要改的……
-    type: 'ajax',
-    format: 'json',
-    method: 'GET',
-    reader: {
-        root: 'vendors',
-        successProperty: 'success',
-        totalProperty: 'totalRecords'
-    },
-    writer: {
-        getRecordData: function(record) {
-            return {
-                user: record.data
+    proxy: {
+        url: 'servlet/GetLogData?type=vendors', //TODO 要改的……
+        type: 'ajax',
+        format: 'json',
+        method: 'GET',
+        reader: {
+            root: 'vendors',
+            successProperty: 'success',
+            totalProperty: 'totalRecords'
+        },
+        writer: {
+            getRecordData: function(record) {
+                return {
+                    user: record.data
+                }
             }
         }
     }
-}
 });
